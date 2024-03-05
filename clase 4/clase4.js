daacff22b158c2222e2223d01741a2a3cd805daa
sebastian.perez <sebastian.perez@sofwareone.com>
2024-03-04 19:10:11 -0500

    update class 4
    4 vectores hechos con el metodo flecha y sus respectivos metodos

--- clase 4/clase4.js
+++ clase 4/clase4.js
@@ -1,4 +1,4 @@
-//Arreglos
+/*//Arreglos
 
 let varios=[1,"juan","bogota",20.45,true];
 // en consola el tamaño del arreglo
@@ -55,9 +55,84 @@ console.log(filtered);
 const saludarPais = paises.map(country=>{
     return "Bienvenidos pasajeros a "+ country;
 });
-console.log(saludarPais);
+console.log(saludarPais);*/
+//-------------------------------------------------------
+const Edades = () => [11,23,13,44,15,16,57];
 
-const numbers = () => [1,2,3,4,5,6,7,8,9];
-
-const numeritos = numbers();
+const numeritos = Edades();
 console.log(numeritos);
+
+let filtrar_edades = numeritos.filter(edad=>{
+    return edad >= 18;
+
+});
+console.log(filtrar_edades);
+//------------------------------------------------------------
+
+const llenarVector = (tamaño,valores) =>{
+    const Vec=[];
+    for(i=0; i<tamaño; i++){
+        Vec.push(valores * i)
+    }
+    return Vec;
+};
+const vector = llenarVector(5,100)
+
+console.log(vector);
+
+let resultado = vector.reduce ((Acumulador,nuemroActual)=>{
+    return (Acumulador + nuemroActual) -3;
+})
+
+console.log(resultado);
+//-----------------------------------------------------------
+
+const Nombres = () => ["pedro","alvaro","santiago","antonella","martin","pepe"]
+
+const names = Nombres();
+console.log(names)
+
+
+let Saludo = names.map(nombre =>{
+     
+    if(nombre.startsWith("a")){
+        return "Hola, un gusto saludarte " + nombre;
+    }
+    else{
+        return "Hola " + nombre + " es un placer saludarte";
+    }
+    
+});
+console.log(Saludo)
+//-------------------------------------------
+const numeroAleatorio = (size, numMinimo, numMaximo)=>{
+    const numbers =[];
+    for(i=0; i<size;i++){
+        numbers.push(Math.floor(Math.random() * (numMaximo - numMinimo +1)) + numMinimo)
+    }
+
+    return numbers;
+};
+const numAleatorio = numeroAleatorio(5,1,100)
+console.log(numAleatorio);
+
+let ordenado = numAleatorio.concat(numeritos);
+console.log(ordenado);
+for(a=0; a<ordenado.length; a++){
+    for(b=0; b<ordenado.length -1;b++){
+        if(ordenado[b]<ordenado[b+1]){
+
+            let Aux = ordenado[b];
+            ordenado[b]=ordenado[b+1];
+            ordenado[b +1] = Aux;
+        }
+    }
+}
+console.log(ordenado);
+
+let Multiplos2 = ordenado.filter(multiplo =>{
+
+    return multiplo % 2 == 0;
+    
+});
+console.log(Multiplos2);
